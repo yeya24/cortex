5dae68bd8184add2bf4fcc7c6fc0117351ae0e38
Bryan Boreham <bryan@weave.works>
2019-09-01 13:50:43 +0000

    Return request data to the pool in Ingester.Push()
    Signed-off-by: Bryan Boreham <bryan@weave.works>

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -275,6 +275,7 @@ func (i *Ingester) Push(ctx old_ctx.Context, req *client.WriteRequest) (*client.
 			return nil, err
 		}
 	}
+	client.ReuseSlice(req.Timeseries)
 
 	return &client.WriteResponse{}, lastPartialErr
 }
