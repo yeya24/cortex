8322ba761fa1f9a08fee46e841889c00193fbcfd
afayek <afayekhassan@gmail.com>
2023-07-13 16:25:18 -0700

    add snappy compression

--- pkg/querier/tripperware/instantquery/instant_query.go
+++ pkg/querier/tripperware/instantquery/instant_query.go
@@ -206,8 +206,7 @@ func (instantQueryCodec) EncodeRequest(ctx context.Context, r tripperware.Reques
 		}
 	}
 
-	// Always ask gzip to the querier
-	h.Set("Accept-Encoding", "gzip")
+	h.Set("Accept-Encoding", "snappy")
 
 	req := &http.Request{
 		Method:     "GET",
