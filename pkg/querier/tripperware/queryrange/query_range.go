6c2ff9004fe73fa9904f34acc4cd5e7d4f948893
afayek <afayekhassan@gmail.com>
2023-08-08 19:54:31 -0700

    return json response for unsharded querier requests

--- pkg/querier/tripperware/queryrange/query_range.go
+++ pkg/querier/tripperware/queryrange/query_range.go
@@ -247,6 +247,7 @@ func (prometheusCodec) EncodeRequest(ctx context.Context, r tripperware.Request)
 	}
 
 	h.Set("Accept-Encoding", "snappy")
+	h.Set("Accept", "application/x-protobuf")
 
 	req := &http.Request{
 		Method:     "GET",
